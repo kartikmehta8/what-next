c313deddd54fcc0cda0e2fc2d6b15bd78b66fe74
Kartik Mehta <kartikmehta202@gmail.com>
2023-08-17 00:47:41 +0530

    recommendation

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
-import React, { useState } from 'react';
-import PurchaseHistory from '/Users/poorvika.gupta/what-next/src/components/product_recmd.js';
-import ProductRecommendation from '/Users/poorvika.gupta/what-next/src/components/product_recmd.js';
-import data from '/Users/poorvika.gupta/what-next/src/data.json';
+import React from 'react';
+import RecommendationEngine from './components/RecommendationEngine';
+import { DATA } from './data';
 
 const App = () => {
-  const [purchaseHistory, setPurchaseHistory] = useState([]);
-  const [currentRecommendationIndex, setCurrentRecommendationIndex] = useState(0);
-
-  const handleBuy = () => {
-    const recommendedProduct = data[currentRecommendationIndex];
-    setPurchaseHistory([...purchaseHistory, recommendedProduct]);
-    setCurrentRecommendationIndex(currentRecommendationIndex + 1);
-  };
-
-  const handleSkip = () => {
-    setCurrentRecommendationIndex(currentRecommendationIndex + 1);
-  };
-
-  const currentRecommendation = data[currentRecommendationIndex];
-
   return (
     <div>
-      <h1>Product Recommendation Game</h1>
-      <PurchaseHistory history={purchaseHistory} />
-      {currentRecommendation ? (
-        <ProductRecommendation
-          product={currentRecommendation}
-          onBuy={handleBuy}
-          onSkip={handleSkip}
-        />
-      ) : (
-        <p>No more recommendations available.</p>
-      )}
+      <div className='flex py-2 px-4 justify-center bg-blue-500 text-white text-xl font-bold poppins'>
+        Recommendation System
+      </div>
+      <div style={{ height: '100vh' }} className='bg-gray-'>
+        <div className='flex justify-center items-center h-full'>
+          <RecommendationEngine data={DATA} />
+        </div>
+      </div>
+      <div className='flex py-2 px-4 justify-center bg-blue-500 text-white text-xl font-bold poppins'>
+        <a
+          href='https://github.com/poorvika11'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='hover:text-black'
+        >
+          @poorvika11
+        </a>
+      </div>
     </div>
   );
 };
 
 export default App;
-
